90cb6f1895ad239e2c6b0dbdfa5f585c2a90559f
falcon256 <256games@gmail.com>
2019-02-11 14:07:02 -0700

    Wireframe shader + mesh serializer noted.
    The mesh serializer isn't the original hololens one, had to modify it so that it could pass the data back since we can't instantiate meshes outside of the main thread. Tossed a wireframe shader on here for now until I can start projecting captured camera goodness.

--- SUITSVRGroundStation/Assets/Scripts/SimpleMeshSerializer.cs
+++ SUITSVRGroundStation/Assets/Scripts/SimpleMeshSerializer.cs
@@ -1,6 +1,22 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+
+
+
+
+
+
+
+//NOTE - HEAVILY MODIFIED!
+//Google up the original instead of using this one if you are not Dan.
+
+
+
+
+
+
+
 using System.Collections.Generic;
 using SysDiag = System.Diagnostics;
 using System.IO;
