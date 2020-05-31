067a40b027b6d4bf74e9e139398b32920d8b262e
Falcon256 <256games@gmail.com>
2020-05-31 16:41:00 -0600

    Added an RPC script to the main scene to work with.

---
+++ SUITSVRGroundStation/Assets/Scripts/PhotonRPCLinks.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+
+public class PhotonRPCLinks : MonoBehaviour
+{
+    public static PhotonRPCLinks singleton = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!singleton)
+            singleton = this;
+        else
+            Debug.LogError("PhotonRPCLinks DUPLICATE SINGLETONS ATTEMPTED!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
